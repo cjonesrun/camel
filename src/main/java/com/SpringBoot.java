0dcf5d3707548c67fa68d52c0e36ba9d80aaf777
chris jones <cjonesrun@gmail.com>
2017-04-04 09:04:27 -0400

    changes

--- src/main/java/com/SpringBoot.java
+++ src/main/java/com/SpringBoot.java
@@ -3,13 +3,12 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import sample.camel.SampleCamelApplication;
 
 // from camel-examples/camel-example-spring-boot
-@SpringBootApplication
+//@SpringBootApplication
 public class SpringBoot {
 	public static void main(String[] args) {
-		SpringApplication.run(SampleCamelApplication.class, args);
+		//SpringApplication.run(SampleCamelApplication.class, args);
 
 	}
 }
