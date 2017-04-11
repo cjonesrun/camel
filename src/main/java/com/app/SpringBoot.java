6b43f458df62220b3869bec83df912544f4302cf
chris jones <cjonesrun@gmail.com>
2017-04-11 08:21:07 -0400

    build fixes

--- src/main/java/com/app/SpringBoot.java
+++ src/main/java/com/app/SpringBoot.java
@@ -1,14 +1,15 @@
-package com;
+package com.app;
 
-import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 // from camel-examples/camel-example-spring-boot
-//@SpringBootApplication
+@SpringBootApplication
 public class SpringBoot {
-	public static void main(String[] args) {
+	public static final void main(String[] args) {
 		//SpringApplication.run(SampleCamelApplication.class, args);
 
+		System.out.println();
 	}
 }
